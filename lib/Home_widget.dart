dd29ea75858513d10591371fd3404bc78642c91b
Mart24 <martijnformer24@gmail.com>
2021-01-11 20:46:34 +0100

    Bottombar

---
+++ lib/Home_widget.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/Pages.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    HomePage(),
+    Voeding(),
+    Voortgang(),
+    Profiel(),
+    Lijstje(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Voedingsapp"),
+      ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: onTabTapped,
+          currentIndex: _currentIndex,
+          items: [
+            BottomNavigationBarItem(icon: Icon(Icons.book), label: "Dagboek"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.restaurant), label: "Voeding"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.leaderboard), label: "Voortgang"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_pin), label: "Profiel"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt), label: "Lijstje"),
+          ]),
+    );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
+  }
+}
---
+++ lib/Pages.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+    );
+  }
+}
+
+class Voeding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+}
+
+class Voortgang extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.orange,
+    );
+  }
+}
+
+class Profiel extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+    );
+  }
+}
+
+class Lijstje extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
